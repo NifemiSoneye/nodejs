2820a3598d0a9904100ca06bb4cccb9954442353
NifemiSoneye <nifemisoneye@gmail.com>
2026-01-26 12:17:20 -0800

    Router chapter started

---
+++ 04tut/routes/api/employees.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+const data = {};
+data.employees = require("../../data/employees.json");
+
+router.route("/").get((req, res) => {
+  res.json(data.employees);
+});
+
+module.exports = router;
