638e1f42b12943067cb4f00ba9db6c5c4be0fc72
NifemiSoneye <nifemisoneye@gmail.com>
2026-01-30 22:54:42 -0800

    LOGOUT HANDLER INITIALIZED

---
+++ 04tut/controllers/logoutController.js
@@ -0,0 +1,35 @@
+const usersDB = {
+  users: require("../model/users.json"),
+  setUsers: function (data) {
+    this.users = data;
+  },
+};
+
+const fsPromises = require("fs").promises;
+const path = require("path");
+
+const handleLogout = (req, res) => {
+  //On client , also delete the accessToken
+
+  const cookies = req.cookies;
+  if (!cookies?.jwt) return res.sendStatus(204); //No content
+  const refreshToken = cookies.jwt;
+
+  //Is refreshToken in db
+  const foundUser = usersDB.users.find(
+    (person) => person.refreshToken === refreshToken,
+  );
+  if (!foundUser) return res.sendStatus(403); // forbidden
+  //evaluate jwt
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
+    if (err || foundUser.username != decoded.username)
+      return res.sendStatus(403);
+    const accessToken = jwt.sign(
+      { username: decoded.username },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "30s" },
+    );
+    res.json({ accessToken });
+  });
+};
+module.exports = { handleRefreshToken };
