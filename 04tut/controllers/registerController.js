b8f909706a37b3b2b34ae6b36cfcd1dd628f6a84
NifemiSoneye <nifemisoneye@gmail.com>
2026-02-23 10:28:58 -0800

    Mongoose schema added and integrated

--- 04tut/controllers/registerController.js
+++ 04tut/controllers/registerController.js
@@ -1,13 +1,5 @@
-const usersDB = {
-  users: require("../model/users.json"),
-  setUsers: function (data) {
-    this.users = data;
-  },
-};
+const User = require("../model/User");
 const bcrypt = require("bcrypt");
-const fsPromises = require("fs").promises;
-const path = require("path");
-
 const handleNewUser = async (req, res) => {
   const { user, pwd } = req.body;
   if (!user || !pwd)
@@ -15,23 +7,25 @@ const handleNewUser = async (req, res) => {
       .status(400)
       .json({ Message: "Username and password are required." });
   // check for duplicate usernames in the db
-  const duplicate = usersDB.users.find((person) => person.username === user);
+  const duplicate = await User.findOne({ username: user }).exec();
   if (duplicate) return res.sendStatus(409);
   try {
     //encrypt password
     const hashedPwd = await bcrypt.hash(pwd, 10);
-    //store new user
-    const newUser = {
+    //create store new user
+    const result = await User.create({
       username: user,
-      roles: { User: 2001 },
       password: hashedPwd,
-    };
-    usersDB.setUsers([...usersDB.users, newUser]);
-    await fsPromises.writeFile(
-      path.join(__dirname, "..", "model", "users.json"),
-      JSON.stringify(usersDB.users),
-    );
-    console.log(usersDB.users);
+    });
+
+    /*     const newUser = new User({
+      username: user,
+      password: hashedPwd,
+    }
+)
+    const result = await newUser.save */
+
+    console.log(result);
     res.status(201).json({ success: `New User ${user} created!` });
   } catch (err) {
     console.error("Error:", err); // Change this to show full error
