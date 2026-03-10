647e078823f80b9eaa11fe8aed37330e84d4998d
NifemiSoneye <nifemisoneye@gmail.com>
2026-03-10 03:00:54 -0700

    Roles added to server response

--- 04tut/config/allowedOrigins.js
+++ 04tut/config/allowedOrigins.js
@@ -2,6 +2,8 @@ const allowedOrigins = [
   "https://nifemisoneye-portfolio.vercel.app/",
   "http://127.0.0.1:5500",
   "http://localhost:3500",
+  "http://localhost:3000",
+  "http://localhost:5173",
 ];
 
 module.exports = allowedOrigins;
