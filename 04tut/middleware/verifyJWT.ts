869107888739adbf86e8f9e7291d9ad39086b8b6
NifemiSoneye <nifemisoneye@gmail.com>
2026-03-13 12:37:29 -0700

    refactored jwt verification

---
+++ 04tut/middleware/verifyJWT.ts
@@ -0,0 +1,36 @@
+import jwt from "jsonwebtoken";
+import { Request, Response, NextFunction } from "express";
+import { verify } from "node:crypto";
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: string;
+      roles?: number[];
+    }
+  }
+}
+
+const verifyJWT = (req: Request, res: Response, next: NextFunction) => {
+  console.log(req.headers);
+  const authHeader =
+    req.headers.authorization || (req.headers.Authorization as string);
+  if (!authHeader?.startsWith("Bearer ")) return res.sendStatus(401);
+  const token = authHeader.split(" ")[1];
+  console.log("Extracted Token:", token);
+  jwt.verify(
+    token,
+    process.env.ACCESS_TOKEN_SECRET as string,
+    (err, decoded) => {
+      if (err) return res.sendStatus(403);
+      const payload = decoded as {
+        UserInfo: { username: string; roles: number[] };
+      };
+      req.user = payload.UserInfo.username;
+      req.roles = payload.UserInfo.roles;
+      next();
+    },
+  );
+};
+
+export default verifyJWT;
