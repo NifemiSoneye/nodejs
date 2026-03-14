cf5bf134f7d890f4f0dab94b489c8cba07642f4c
NifemiSoneye <nifemisoneye@gmail.com>
2026-03-14 01:32:57 -0700

    Root and auth routes refactored

---
+++ 04tut/routes/root.ts
@@ -0,0 +1,9 @@
+import express, { Request, Response } from "express";
+import path from "path";
+const router = express.Router();
+
+router.get(/^\/$|\/index(\.html)?$/, (req: Request, res: Response): void => {
+  res.sendFile(path.join(__dirname, "..", "views", "index.html"));
+});
+
+export default router;
