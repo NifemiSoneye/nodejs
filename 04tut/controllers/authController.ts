9e8322fcd5707a90b61a183a835fd81afa6ad638
NifemiSoneye <nifemisoneye@gmail.com>
2026-03-18 07:05:09 -0700

    Refactored auth controller and logout controller

--- 04tut/controllers/authController.ts
+++ 04tut/controllers/authController.ts
@@ -4,6 +4,7 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 const handleLogin = async (req: Request, res: Response) => {
+  const cookies = req.cookies;
   const { user, pwd }: { user: string; pwd: string } = req.body;
   if (!user || !pwd)
     return res
@@ -26,16 +27,27 @@ const handleLogin = async (req: Request, res: Response) => {
       process.env.ACCESS_TOKEN_SECRET as string,
       { expiresIn: "60s" },
     );
-    const refreshToken = jwt.sign(
+    const newRefreshToken = jwt.sign(
       { username: foundUser.username },
       process.env.REFRESH_TOKEN_SECRET as string,
       { expiresIn: "15m" },
     );
+
+    const newRefreshTokenArray = !cookies?.jwt
+      ? (foundUser.refreshToken ?? [])
+      : (foundUser.refreshToken?.filter((rt) => rt !== cookies.jwt) ?? []);
+
+    if (cookies.jwt)
+      res.clearCookie("jwt", {
+        httpOnly: true,
+        sameSite: "lax",
+        secure: false,
+      });
     //Saving refreshToken with current user
-    foundUser.refreshToken = refreshToken;
+    foundUser.refreshToken = [...newRefreshTokenArray, newRefreshToken];
     const result = await foundUser.save();
     console.log(result);
-    res.cookie("jwt", refreshToken, {
+    res.cookie("jwt", newRefreshToken, {
       httpOnly: true,
       sameSite: "lax",
       secure: false,
--- 04tut/controllers/logoutController.ts
+++ 04tut/controllers/logoutController.ts
@@ -17,7 +17,8 @@ const handleLogout = async (req: Request, res: Response) => {
 
   //Delete refreshToken in db
 
-  foundUser.refreshToken = "";
+  foundUser.refreshToken =
+    foundUser.refreshToken?.filter((rt) => rt !== refreshToken) ?? [];
   const result = await foundUser.save();
   console.log(result);
 
