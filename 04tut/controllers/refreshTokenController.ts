e8cd1dcc340bd7483d55211222a3d5e7f6f5f079
NifemiSoneye <nifemisoneye@gmail.com>
2026-03-18 06:52:22 -0700

    Added reuse and rotation to refresh token controller

--- 04tut/controllers/refreshTokenController.ts
+++ 04tut/controllers/refreshTokenController.ts
@@ -11,20 +11,49 @@ const handleRefreshToken = async (req: Request, res: Response) => {
   const foundUser = await User.findOne({ refreshToken }).exec();
 
   //Detected refresh token reuse
-  if (!foundUser) return res.sendStatus(403); // forbidden
+  if (!foundUser) {
+    jwt.verify(
+      refreshToken,
+      process.env.REFRESH_TOKEN_SECRET as string,
+      async (
+        err: jwt.VerifyErrors | null,
+        decoded: jwt.JwtPayload | string | undefined,
+      ) => {
+        if (!decoded || typeof decoded === "string") return res.sendStatus(403);
+        if (err) return res.sendStatus(403);
+        const hackedUser = await User.findOne({
+          username: decoded.username,
+        }).exec();
+        if (!hackedUser) return;
+        hackedUser.refreshToken = [];
+        const result = await hackedUser.save();
+        console.log(result);
+      },
+    );
+    return res.sendStatus(403);
+  }
+
+  const newRefreshTokenArray =
+    foundUser.refreshToken?.filter((rt) => rt !== refreshToken) ?? [];
   //evaluate jwt
   jwt.verify(
     refreshToken,
     process.env.REFRESH_TOKEN_SECRET as string,
-    (
+    async (
       err: jwt.VerifyErrors | null,
       decoded: jwt.JwtPayload | string | undefined,
     ) => {
+      if (err) {
+        foundUser.refreshToken = [...newRefreshTokenArray];
+        const result = await foundUser.save();
+      }
       const payload = decoded as {
         UserInfo: { username: string; roles: number[] };
       };
       if (err || foundUser.username != payload.UserInfo.username)
         return res.sendStatus(403);
+
+      // Refresh token was still valid
       const roles = Object.values(foundUser.roles);
       const accessToken = jwt.sign(
         {
@@ -36,6 +65,22 @@ const handleRefreshToken = async (req: Request, res: Response) => {
         process.env.ACCESS_TOKEN_SECRET as string,
         { expiresIn: "30s" },
       );
+
+      const newRefreshToken = jwt.sign(
+        { username: foundUser.username },
+        process.env.REFRESH_TOKEN_SECRET as string,
+        { expiresIn: "15m" },
+      );
+      //Saving refreshToken with current user
+      foundUser.refreshToken = [...newRefreshTokenArray, newRefreshToken];
+      const result = await foundUser.save();
+
+      res.cookie("jwt", newRefreshToken, {
+        httpOnly: true,
+        sameSite: "lax",
+        secure: false,
+        maxAge: 24 * 60 * 60 * 1000,
+      });
       res.json({ accessToken });
     },
   );
